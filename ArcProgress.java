0040d1671c45184d78d374e9aea2ece0eda4daa5
manuel201728 <55290607+manuel201728@users.noreply.github.com>
2022-05-12 11:19:16 -0400

    Update ArcProgress.java

--- ArcProgress.java
+++ ArcProgress.java
@@ -269,7 +269,7 @@ public class ArcProgress extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         float startAngle = 270 - arcAngle / 2f;
-        float finishedSweepAngle = progress / (float) getMax() * arcAngle;
+        float finishedSweepAngle = progress * arcAngle;
         float finishedStartAngle = startAngle;
         if(progress == 0) finishedStartAngle = 0.01f;
         paint.setColor(unfinishedStrokeColor);
